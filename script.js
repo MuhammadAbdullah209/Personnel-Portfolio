22fe212e9a572fb0aa8f14541a70ebb0846a2fdc
M.Abdullah <abdullah488838@gmail.com>
2026-05-08 21:00:19 +0500

    hamburger added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 document.documentElement.style.scrollBehavior = "smooth";
+
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    // Toggle hamburger ↔ cross
+    if (hamburger.classList.contains("active")) {
+        hamburger.classList.remove("active");
+        hamburger.innerHTML = "☰"; // hamburger icon
+    } else {
+        hamburger.classList.add("active");
+        hamburger.innerHTML = "✖"; // cross icon
+    }
+});
